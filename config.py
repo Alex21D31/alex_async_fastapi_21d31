aaa4e71e6a403c9705315b7c5c95258789a1d53f
Alex21D31 <workalex6644@gmail.com>
2026-03-27 17:49:01 +0300

    update yaml and config file, add: redis.

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ class Settings(BaseSettings):
     SECRET_KEY : str
     ALGORITHM : str
     LIVE_TIME : int
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
     model_config = {"env_file": ".env"}
 settings = Settings()  
