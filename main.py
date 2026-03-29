097626dd938d319dbb1396da264e5d6f1712e56c
Alex21D31 <workalex6644@gmail.com>
2026-03-29 15:26:01 +0300

    add: redis RateLimited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from middleware import LogMiddleware
+from middleware import LogMiddleware, RateLimitMiddleware
 from routers import auth, admins, users, orders, products
 from models import User, Product, Order, OrderItem
 from services.redis_service import redis_service
@@ -13,6 +13,7 @@ async def lifespan(app : FastAPI):
     yield
     await redis_service.close()
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(RateLimitMiddleware)
 app.add_middleware(LogMiddleware)
 app.include_router(auth.router)
 app.include_router(users.router)
--- middleware.py
+++ middleware.py
@@ -1,8 +1,17 @@
 import logging
 from fastapi import Request
+from fastapi.responses import JSONResponse
+from services.redis_service import redis_service
+from datetime import datetime, timezone
 from starlette.middleware.base import BaseHTTPMiddleware
+import uuid
 
 logger = logging.getLogger(__name__)
+
+EXCLUDED_PATHS = ['/products', '/openapi.json', '/docs', '/redoc']
+
+LIMIT = 5
+WINDOW = 30
 
 class LogMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request : Request, call_next):
@@ -13,3 +22,22 @@ class LogMiddleware(BaseHTTPMiddleware):
         else:
             logger.info(f'<- {response.status_code} {request.method} {request.url}')
         return response
+class RateLimitMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        if any(request.url.path.startswith(path) for path in EXCLUDED_PATHS):
+            return await call_next(request)
+        ip = request.client.host
+        key = f'rate_limit:{ip}'
+        now = datetime.now(timezone.utc).timestamp() 
+        window_start = now - 30
+        member = str(uuid.uuid4())
+        try:
+            await redis_service.redis_client.zremrangebyscore(key, 0 , window_start)
+            await redis_service.redis_client.zadd(key, {member : now})
+            result = await redis_service.redis_client.zcard(key)
+            if result > LIMIT:
+                return JSONResponse(status_code=429, content={'detail': 'Слишком много запросов. Подождите 30 секунд.'})
+        except Exception as e:
+            logger.error(f'Ошибка Redis в rate limiter: {e}')
+        return await call_next(request)
+            
