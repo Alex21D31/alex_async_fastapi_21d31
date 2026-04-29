956204912253c96fbaf36ea540ac07e67671b2fd
Alex21D31 <workalex6644@gmail.com>
2026-04-29 10:57:17 +0300

    update: models

--- models.py
+++ models.py
@@ -8,10 +8,20 @@ class Role(enum.Enum):
     admin = 'admin'
     creator = 'creator'
     banned = 'banned'
+    seller = 'seller'
 class Status(enum.Enum):
     pending = 'pending'
     delivered = 'delivered'
     cancelled = 'cancelled'
+class TaskStage (enum.Enum):
+    pending = 'pending'
+    processing = 'processing'
+    shipping = 'shipping'
+    delivered = 'delivered'
+class ApplicationStatus(enum.Enum):
+    pending = 'pending'
+    approved = 'approved'
+    rejected = 'rejected'
 class User(Base):
     __tablename__ = 'users'
     id : Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
@@ -30,6 +40,7 @@ class Product(Base):
     description : Mapped[str | None]
     price : Mapped[int] = mapped_column(nullable=False)
     quantity : Mapped[int] = mapped_column(nullable=False)
+    category_id : Mapped[int | None] = mapped_column(ForeignKey('categories.id'), nullable=True)
     created_at : Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at : Mapped[datetime] = mapped_column(nullable=True,onupdate=func.now())
     items : Mapped[list['OrderItem']] = relationship('OrderItem', back_populates='product', cascade='all, delete-orphan')
@@ -41,6 +52,8 @@ class Order(Base):
     updated_at : Mapped[datetime] = mapped_column(nullable=True,onupdate=func.now())
     status : Mapped[str] = mapped_column(Enum(Status), default=Status.pending)
     user_id : Mapped[int] = mapped_column(ForeignKey('users.id'))
+    task_id : Mapped[str | None] = mapped_column(nullable=True)
+    task_stage : Mapped[str] = mapped_column(Enum(TaskStage),default=TaskStage.pending)
     user : Mapped['User'] = relationship('User', back_populates='orders')
     items : Mapped[list['OrderItem']] = relationship('OrderItem', back_populates='order',cascade='all, delete-orphan', lazy='selectin')
 class OrderItem(Base):
@@ -51,3 +64,35 @@ class OrderItem(Base):
     quantity : Mapped[int]
     order : Mapped['Order'] = relationship('Order', back_populates='items')
     product : Mapped['Product'] = relationship('Product', back_populates='items',lazy='selectin')
+
+class Category(Base):
+    __tablename__ = 'categories'
+    id : Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name : Mapped[str] = mapped_column(nullable=False, unique=True)
+    description : Mapped[str | None] = mapped_column(nullable=True)
+class Shop(Base):
+    __tablename__ =  'shops'
+    id : Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name : Mapped[str] = mapped_column(nullable=False, unique=True)
+    description : Mapped[str | None] = mapped_column(nullable=True)
+    seller_id : Mapped[int] = mapped_column(ForeignKey('users.id'))
+    is_verified : Mapped[bool] = mapped_column(default=False)
+    created_at : Mapped[datetime] = mapped_column(server_default=func.now())
+class ShopProduct(Base):
+    __tablename__ = 'shopproducts'
+    id : Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    quantity : Mapped[int] = mapped_column(nullable=False)
+    price : Mapped[int] = mapped_column(nullable=False)
+    shop_id : Mapped[int] = mapped_column(ForeignKey('shops.id'))
+    product_id : Mapped[int] = mapped_column(ForeignKey('products.id'))
+    category_id : Mapped[int] = mapped_column(ForeignKey('categories.id'))
+class SellerApplication(Base):
+    __tablename__ = 'sellerapplications'
+    id : Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    text : Mapped[str | None] = mapped_column(nullable=True)
+    status : Mapped[str] = mapped_column(Enum(ApplicationStatus),default=ApplicationStatus.pending)
+    created_at : Mapped[datetime] = mapped_column(server_default=func.now())
+    user_id : Mapped[int] = mapped_column(ForeignKey('users.id'))
+    reviewed_by : Mapped[int | None] = mapped_column(ForeignKey('users.id'), nullable=True, default=None)
+
+ 
